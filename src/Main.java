6fd13b3bd833a62deda4055cc31861dc09bf37c8
kt-NicoHartmann <nico.hartmann@karrieretutor.de>
2026-08-07 10:32:45 +0200

    Loop Feature eingebaut

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         System.out.println("Hallo Welt!");
         System.out.println("Hallo Welt!");
 
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
     }
 }
